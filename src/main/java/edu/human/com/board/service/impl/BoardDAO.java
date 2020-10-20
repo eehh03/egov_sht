88c092d547f6980427a3a482810f14ad0d9feab1
s2j55 <s2jeongyang@gmail.com>
2020-10-20 15:55:19 +0900

    디자인 수정

---
+++ src/main/java/edu/human/com/board/service/impl/BoardDAO.java
@@ -0,0 +1,13 @@
+package edu.human.com.board.service.impl;
+
+import org.springframework.stereotype.Repository;
+
+import edu.human.com.common.EgovComAbstractMapper;
+
+@Repository
+public class BoardDAO extends EgovComAbstractMapper {
+	public void deleteBoard(long NTT_ID) {
+		delete("boardMapper.deleteBoard",NTT_ID);
+	}
+
+}
